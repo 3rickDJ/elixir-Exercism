b10fc4b60318cd74147a5b95796eb40fa8eadea8
3rickDJ <erickdejesusmolina@gmail.com>
2023-03-26 13:23:31 -0600

    partial solve ex7

--- language-list/lib/language_list.ex
+++ language-list/lib/language_list.ex
@@ -1,25 +1,17 @@
 defmodule LanguageList do
-  def new() do
-    # Please implement the new/0 function
-  end
+  def new(), do: []
 
-  def add(list, language) do
-    # Please implement the add/2 function
-  end
+  def add(list, language), do: [language, list]
 
-  def remove(list) do
-    # Please implement the remove/1 function
-  end
+  def remove([_head, list]), do: list
 
-  def first(list) do
-    # Please implement the first/1 function
-  end
+  def first([head, _list]), do: head
 
-  def count(list) do
-    # Please implement the count/1 function
-  end
+  def count(list), do: counter(list, 0)
 
-  def functional_list?(list) do
-    # Please implement the functional_list?/1 function
-  end
+  defp counter([_head, tail], counter), do: counter(tail, counter + 1)
+
+  defp counter([], counter), do: counter
+
+  def functional_list?(list), do: "Elixir" in list
 end

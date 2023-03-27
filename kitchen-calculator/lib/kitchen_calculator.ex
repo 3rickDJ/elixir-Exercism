ddf852d47a402963b3ab1cead89959e8b3d06d9b
3rickDJ <erickdejesusmolina@gmail.com>
2023-03-27 09:38:50 -0600

    second advance

--- kitchen-calculator/lib/kitchen_calculator.ex
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,30 +1,22 @@
 defmodule KitchenCalculator do
-  def get_volume({:cup, vol}), do: vol
+  def get_volume({_unit, vol}), do: vol
 
   @conversion %{
     cup: 240.0,
     fluid_ounce: 30.0,
     teaspoon: 5.0,
     tablespoon: 15.0,
-    milimiter: 1.0
+    milliliter: 1.0
   }
-  def to_milimiter({unit, qty}) do
-    @conversion[unit] * qty
+  def to_milliliter({unit, qty}) do
+    {:milliliter, @conversion[unit] * qty}
   end
 
-  def to_milliliter({:cup, qty}), do: {:milimiter, qty * 240.0}
-  def to_milliliter({:fluid_ounce, qty}), do: qty * 30.0
-  def to_milliliter({:teaspoon, qty}), do: qty * 5.0
-  def to_milliliter({:tablespoon, qty}), do: qty * 15.0
-  def to_milliliter({:milliliter, qty}), do: qty * 1.0
+  def from_milliliter({:milliliter, qty}, unit) do
+    {unit, qty/@conversion[unit]}
+  end
 
-  def from_milliliter(volume_pair, :cup), do: to_milliliter(volume_pair) / 240.0
-  def from_milliliter(volume_pair, :fluid_ounce), do: to_milliliter(volume_pair) / 30.0
-  def from_milliliter(volume_pair, :teaspoon), do: to_milliliter(volume_pair) / 5.0
-  def from_milliliter(volume_pair, :tablespoon), do: to_milliliter(volume_pair) / 15.0
-  def from_milliliter(volume_pair, :milimiter), do: to_milliliter(volume_pair) / 1.0
-
-  def convert(volume_pair, unit) do
-    # Please implement the convert/2 function
+  def convert({unit_from, qty}, unit) do
+    nil
   end
 end

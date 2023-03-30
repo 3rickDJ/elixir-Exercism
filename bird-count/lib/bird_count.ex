fb7a23948261abc6b9c130ebd4b9e9a9d2e5efbb
3rickDJ <erickdejesusmolina@gmail.com>
2023-03-30 12:49:35 -0600

    refactor: public to private functions

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -3,26 +3,25 @@ defmodule BirdCount do
   def today([head | _tail]), do: head
 
   def increment_day_count([]), do: [1]
-  def increment_day_count([head | tail]), do: [head + 1| tail]
+  def increment_day_count([head | tail]), do: [head + 1 | tail]
 
   def has_day_without_birds?(list) do
     _has_day_without_birds?(list, false)
   end
 
-  def _has_day_without_birds?(_, true), do: true
-  def _has_day_without_birds?([], birds), do: birds
+  defp _has_day_without_birds?(_, true), do: true
+  defp _has_day_without_birds?([], birds), do: birds
 
-  def _has_day_without_birds?([head | tail], birds) do
-    _has_day_without_birds?(tail, birds or head == 0)
-  end
+  defp _has_day_without_birds?([head | tail], birds),
+    do: _has_day_without_birds?(tail, birds or head == 0)
 
   def total(list) do
     _total(list, 0)
   end
 
-  def _total([], ctr), do: ctr
+  defp _total([], ctr), do: ctr
 
-  def _total([head | tail], ctr) do
+  defp _total([head | tail], ctr) do
     _total(tail, ctr + head)
   end
 
@@ -30,7 +29,7 @@ defmodule BirdCount do
     _busy_days(list, 0)
   end
 
-  def _busy_days([head | tail], ctr) when head < 5, do: _busy_days(tail, ctr)
-  def _busy_days([], ctr), do: ctr
-  def _busy_days([_head | tail], ctr), do: _busy_days(tail, ctr + 1)
+  defp _busy_days([head | tail], ctr) when head < 5, do: _busy_days(tail, ctr)
+  defp _busy_days([], ctr), do: ctr
+  defp _busy_days([_head | tail], ctr), do: _busy_days(tail, ctr + 1)
 end

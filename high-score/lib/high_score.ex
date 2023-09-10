014142599e908916e7fe4a489f813538e80299b0
3rickDJ <erickdejesusmolina@gmail.com>
2023-09-10 16:05:59 -0600

    refactor: HighScore use module attributes

--- high-score/lib/high_score.ex
+++ high-score/lib/high_score.ex
@@ -1,19 +1,18 @@
 defmodule HighScore do
+  @default_score 0
+
   def new(), do: %{}
 
-  def add_player(scores, name, score \\ 0), do: Map.put_new(scores, name, score)
+  def add_player(scores, name, score \\ @default_score), do: Map.put_new(scores, name, score)
 
   def remove_player(scores, name), do: Map.delete(scores, name)
 
-  def reset_score(scores, name), do: scores |> Map.put(name, 0)
+  def reset_score(scores, name), do: scores |> Map.put(name, @default_score)
 
   def update_score(scores, name, score) do
     scores
     |> Map.update(name, score, fn current_val -> current_val + score end)
   end
 
-  def get_players(scores) do
-    scores
-    |> Map.keys()
-  end
+  def get_players(scores), do: scores |> Map.keys()
 end

2ef28dfcc63147e8c986c2036f7649f4ee521068
3rickDJ <erickdejesusmolina@gmail.com>
2023-09-11 15:21:25 -0600

    test: #raise positive rational to power of zero

--- rational-numbers/test/rational_numbers_test.exs
+++ rational-numbers/test/rational_numbers_test.exs
@@ -167,7 +167,7 @@ defmodule RationalNumbersTest do
       assert RationalNumbers.pow_rational({1, 1}, 4) == {1, 1}
     end
 
-    @tag :pending
+    # @tag :pending
     test "Raise a positive rational number to the power of zero" do
       assert RationalNumbers.pow_rational({1, 2}, 0) == {1, 1}
     end

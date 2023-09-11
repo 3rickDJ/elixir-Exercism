f71ffddf2b3ffe0f219895f4396e1c59d6ee3466
3rickDJ <erickdejesusmolina@gmail.com>
2023-09-11 12:32:48 -0600

    test: #multiply a rational number by uts reciprocal

--- rational-numbers/test/rational_numbers_test.exs
+++ rational-numbers/test/rational_numbers_test.exs
@@ -61,7 +61,7 @@ defmodule RationalNumbersTest do
       assert RationalNumbers.multiply({-1, 2}, {-2, 3}) == {1, 3}
     end
 
-    @tag :pending
+    # @tag :pending
     test "Multiply a rational number by its reciprocal" do
       assert RationalNumbers.multiply({1, 2}, {2, 1}) == {1, 1}
     end
